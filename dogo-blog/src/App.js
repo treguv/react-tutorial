931dc220e0e34fd873ae92a5890558b3a6117e6a
Vlad Tregubov <vladislavtregubov00@gmail.com>
2021-04-29 00:06:23 -0700

    created basic setup file

---
+++ dogo-blog/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  const title = "Welcome to the maxed out blog";
+  const likes = 50;
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
