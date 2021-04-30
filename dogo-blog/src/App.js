c2a149a4d5452d6a8dc7bdbc0dfef44865581ac8
Vlad Tregubov <vladislavtregubov00@gmail.com>
2021-04-29 23:10:18 -0700

    adds navbar and home components as well as basic page style

--- dogo-blog/src/App.js
+++ dogo-blog/src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
 
+import Navbar from "./Navbar";
+import Home from "./Home";
 function App() {
   const title = "Welcome to the maxed out blog";
   const likes = 50;
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{title}</h1>
+        <Home></Home>
         <p>Liked {likes} times</p>
       </div>
     </div>
---
+++ dogo-blog/src/Home.js
@@ -0,0 +1,10 @@
+import react from "react";
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>HomePage</h2>
+    </div>
+  );
+};
+
+export default Home;
